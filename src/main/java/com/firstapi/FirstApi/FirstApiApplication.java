b459b28ece52e91cb14925ebf06772dad2054fdd
rohitjha17092 <rohit.jha@ripplehire.com>
2025-01-25 06:43:26 -0500

    updated

--- src/main/java/com/firstapi/FirstApi/FirstApiApplication.java
+++ src/main/java/com/firstapi/FirstApi/FirstApiApplication.java
@@ -2,11 +2,19 @@ package com.firstapi.FirstApi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class FirstApiApplication {
+public class FirstApiApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(FirstApiApplication.class);
+	}
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(FirstApiApplication.class, args);
 	}
 
